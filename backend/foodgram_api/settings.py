a52bc75907be8b6d8d1fe2f19c50f537aba47e17
PatimatN <patimat.n.m@yandex.ru>
2021-08-16 14:24:10 +0300

    AllowAny to watch user's page

--- backend/foodgram_api/settings.py
+++ backend/foodgram_api/settings.py
@@ -147,4 +147,19 @@ DJOSER = {
         'user': 'users.serializers.UserShowSerializer',
         'current_user': 'users.serializers.UserShowSerializer'
     },
+    'PERMISSIONS': {
+        'activation': ['rest_framework.permissions.AllowAny'],
+        'password_reset': ['rest_framework.permissions.AllowAny'],
+        'password_reset_confirm': ['rest_framework.permissions.AllowAny'],
+        'set_password': ['djoser.permissions.CurrentUserOrAdmin'],
+        'username_reset': ['rest_framework.permissions.AllowAny'],
+        'username_reset_confirm': ['rest_framework.permissions.AllowAny'],
+        'set_username': ['djoser.permissions.CurrentUserOrAdmin'],
+        'user_create': ['rest_framework.permissions.AllowAny'],
+        'user_delete': ['djoser.permissions.CurrentUserOrAdmin'],
+        'user': ['rest_framework.permissions.AllowAny'],
+        'user_list': ['rest_framework.permissions.AllowAny'],
+        'token_create': ['rest_framework.permissions.AllowAny'],
+        'token_destroy': ['rest_framework.permissions.IsAuthenticated'],
+    }
 }
