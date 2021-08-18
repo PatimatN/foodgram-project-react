30450bc0b9d5da93edb4f2e35ad3ad271036235a
PatimatN <patimat.n.m@yandex.ru>
2021-08-18 16:39:43 +0300

    fix

--- backend/recipes/serializers.py
+++ backend/recipes/serializers.py
@@ -62,14 +62,14 @@ class RecipeCreateSerializer(serializers.ModelSerializer):
         recipe = Recipe.objects.create(**validated_data)
         recipe.tags.set(tags)
         for ingredient in ingredients:
-            ingredient = get_object_or_404(Ingredient, id=ingredient['id'])
+            ingredient_obj = get_object_or_404(Ingredient, id=ingredient['id'])
             amount = ingredient['amount']
             if RecipeIngredient(recipe=recipe,
-                                ingredient=ingredient).objects.exists():
+                                ingredient=ingredient_obj).objects.exists():
                 amount += F('amount')
             RecipeIngredient.objects.update_or_create(
                 recipe=recipe,
-                ingredient=ingredient,
+                ingredient=ingredient_obj,
                 defaults={'amount': amount}
             )
         return recipe
@@ -85,14 +85,16 @@ class RecipeCreateSerializer(serializers.ModelSerializer):
                                                       'отрицательным числом.')
             instance.ingredients.clear()
             for ingredient in ingredients:
-                ingredient = get_object_or_404(Ingredient, id=ingredient['id'])
+                ingredient_obj = get_object_or_404(Ingredient,
+                                                   id=ingredient['id'])
                 amount = ingredient['amount']
                 if RecipeIngredient(recipe=instance,
-                                    ingredient=ingredient).objects.exists():
+                                    ingredient=ingredient_obj).objects\
+                        .exists():
                     amount += F('amount')
                 RecipeIngredient.objects.update_or_create(
                     recipe=instance,
-                    ingredient=ingredient,
+                    ingredient=ingredient_obj,
                     defaults={'amount': amount}
                 )
         if 'tags' in self.initial_data:
